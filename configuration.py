b8b31020aaa4306285534e3ac79679dfa3a6744a
Alexander Shipov <a.shipov935@gmail.com>
2025-09-20 14:28:28 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://c08a636e-c96b-4f00-a301-e641c34f0833.serverhub.praktikum-services.ru"
+CREATE_ORDER = "/api/v1/orders"
+FIND_ORDER_FROM_TRACK = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,17 @@
+headers = {
+    "Content-Type": "application/json",
+}
+
+order_body = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 7,
+    "phone": "+7 900 320 80 90",
+    "rentTime": 5,
+    "deliveryDate": "2025-09-20",
+    "comment": "Saske, come back to Konoha",
+    "color": ["BLACK"]
+}
+
+status_code_200 = 200
