b823f323b6053fc5a69b59cd9caf52fe58eb8fe5
Alexander Shipov <a.shipov935@gmail.com>
2025-09-20 23:47:26 +0300

    Update create_order_get_order_test.py

--- create_order_get_order_test.py
+++ create_order_get_order_test.py
@@ -1,34 +1,9 @@
 # Шипов Александр, 34-я когорта — Финальный проект. Инженер по тестированию плюс
-import requests
-import configuration
 import data
-
-#   Создаем заказ
-
-
-def create_order(order_body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER,
-                         json=order_body,
-                         headers=data.headers)
-
-#   Получаем заказ по трек-номеру
-
-
-def get_order_from_track(track):
-    return requests.get(configuration.URL_SERVICE + configuration.FIND_ORDER_FROM_TRACK + str(track),
-                        headers=data.headers)
-
-
-#   Выполняем запрос на получение заказа по треку
-
-
-def assert_code_200():
-    response_pno = create_order(data.order_body)
-    track = response_pno.json()["track"]
-    return get_order_from_track(track).status_code
+import create_order
 
 # Тест: Проверить, что код ответа 200
 
 
 def test_get_order_from_track_code_200():
-    assert assert_code_200() == data.status_code_200
+    assert create_order.assert_code_200() == data.status_code_200
